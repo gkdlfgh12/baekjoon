ad1818813cc8a7e6a6bb52d5705ead44f63821ea
gkdlfgh12 <gkdlfgh12@naver.com>
2022-06-22 03:24:50 +0900

    dev 1

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -16,7 +16,7 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		//dго╬о~~~~
 		String str = br.readLine();
 		StringTokenizer st = new StringTokenizer(str);
 		boolean flag = true;
