16bcb1f9cb7a8c66b25205e77a10d85983344c5c
gkdlfgh12 <gkdlfgh12@naver.com>
2022-06-22 02:19:19 +0900

    baekjoon 1

---
+++ baekjoon/src/baekjoon/Main.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/*
+ 	3 16
+ ------------
+	3
+	5
+	7
+	11
+	13
+ */
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		boolean flag = true;
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		for(int i=N;i<=M;i++) {
+			if(i != 1) {
+				flag = true;
+				for(int j=2;j<=i-1;j++) {
+					if(i%j == 0) {
+						flag = false;
+						break;
+					}
+				}
+				if(flag) {
+					System.out.println(i);
+				}
+			}
+		}
+	}
+}
---
+++ baekjoon/src/baekjoon/test.java
@@ -0,0 +1,9 @@
+package baekjoon;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println(12/4);
+	}
+
+}
